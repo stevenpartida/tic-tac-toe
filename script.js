082c2445e68c8dcfd0c4bafc36b0bf3affdc76ca
Steven <106054873+stevenpartida@users.noreply.github.com>
2025-10-27 22:13:02 -0700

    Finished first part of console logic.

---
+++ script.js
@@ -0,0 +1,70 @@
+function GameBoard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell());
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placeMarker = (row, column, player) => {
+    if (board[row][column] !== 0) return;
+    board[row][column] = player.marker;
+  };
+
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) =>
+      row.map((cell) => cell.getValue())
+    );
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, printBoard, placeMarker };
+}
+
+function Cell() {
+  let value = 0;
+
+  const addMark = (player) => {
+    value = player;
+  };
+
+  const getValue = () => value;
+
+  return { addMark, getValue };
+}
+
+function GameController(
+  PlayerOneName = "Player One",
+  PlayerTwoName = "Player Two"
+) {
+  const board = GameBoard();
+
+  const players = [
+    {
+      name: playerOneName,
+      marker: "X",
+    },
+    {
+      name: playerTwoName,
+      marker: "O",
+    },
+  ];
+
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
+  const getActivePlayer = () => activePlayer;
+}
+
+const game = GameBoard();
+
+game.printBoard();
